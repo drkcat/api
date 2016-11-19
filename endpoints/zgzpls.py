db3e2d3533a8e6e8646d0aec293d5c84df1dda0a
Ender <luksi.reiku@gmail.com>
2016-11-19 14:41:52 +0100

    0.1.4 Fixed bug with buses like N2 or C1

--- endpoints/zgzpls.py
+++ endpoints/zgzpls.py
@@ -22,7 +22,7 @@ def get_buses(poste:int, source=None):
 
         for row in table:
             cells = row.findAll('td')
-            line = int(cells[0].getText())
+            line = cells[0].getText()
             destination = cells[1].getText().title()
             try:
                 time = int(cells[2].getText().split()[0])
@@ -72,7 +72,7 @@ def get_buses(poste:int, source=None):
             try:
                 time = int(destination['primero'].replace(' minutos', '').rstrip('.'))
                 buses.append({
-                    'line': int(destination['linea']),
+                    'line': destination['linea'],
                     'destination': destination['destino'].rstrip(',').rstrip('.').title(),
                     'time': '{} min.'.format(time)
                 })
@@ -80,7 +80,7 @@ def get_buses(poste:int, source=None):
             except:
                 time = destination['primero'].rstrip('.').replace('cin', 'ción')
                 nodatabuses.append({
-                    'line': int(destination['linea']),
+                    'line': destination['linea'],
                     'destination': destination['destino'].rstrip(',').rstrip('.').title(),
                     'time': '{}'.format(time)
                 })
@@ -88,7 +88,7 @@ def get_buses(poste:int, source=None):
             try:
                 time = int(destination['segundo'].replace(' minutos', '').rstrip('.'))
                 buses.append({
-                    'line': int(destination['linea']),
+                    'line': destination['linea'],
                     'destination': destination['destino'].rstrip(',').rstrip('.').title(),
                     'time': '{} min.'.format(time)
                 })
@@ -96,7 +96,7 @@ def get_buses(poste:int, source=None):
             except:
                 time = destination['segundo'].rstrip('.').replace('cin', 'ción')
                 nodatabuses.append({
-                    'line': int(destination['linea']),
+                    'line': destination['linea'],
                     'destination': destination['destino'].rstrip(',').rstrip('.').title(),
                     'time': '{}'.format(time)
                 })
