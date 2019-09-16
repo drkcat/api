4c00376d61ac0b515b82148d259390f5f6f3b3b6
Ender <luksireiku@icloud.com>
2019-09-16 14:20:10 +0200

    Updated dependencies

--- api.py
+++ api.py
@@ -3,6 +3,8 @@ from falcon import HTTP_400
 from endpoints import zgzpls
 from endpoints import riotpls
 
+app = hug.API(__name__)
+app.http.add_middleware(hug.middleware.CORSMiddleware(app, max_age=7))
 
 @hug.extend_api('/zgzpls')
 def zgzpls_api():
